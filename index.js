24b4f99fc69f503b56f48ac9700272672213ce3d
Yehor Tkachuk <tkachuk.egor2010@gmail.com>
2024-08-17 01:41:27 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+
+
+// let blueText = document.querySelector(".blue-text")
+
+
+// let blueTextContent = blueText.textContent
+
+// let blueButton = document.querySelector(".blue-button").textContent= blueTextContent
+
+
+
+
+
+
+// let imgPre = document.querySelector("img")
+
+// imgPre.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT12WhEQ-uk5sPODDRXtCN9qHmBWbw68pDhuA&s"
+
+
+
+let list = document.querySelector(".list")
+
+list.firstChild.textContent = "owo"
+
